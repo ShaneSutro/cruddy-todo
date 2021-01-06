1271a8c1c0eeb61328982b99138dfaf2a8ca2b79
Josh Parker <joshBBparker@gmail.com>
2021-01-05 20:31:06 -0700

    Complete retrieving one todo

--- datastore/index.js
+++ datastore/index.js
@@ -31,12 +31,13 @@ exports.readAll = (callback) => {
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  fs.readFile(exports.dataDir + `/${id}.txt`, 'utf8', (err, data) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback(null, { id, text: data });
+    }
+  });
 };
 
 exports.update = (id, text, callback) => {
